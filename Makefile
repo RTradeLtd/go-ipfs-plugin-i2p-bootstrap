5358f3ade72bebb168e7ac38d971357911b965e0
dev@dev.dev <dev@dev.dev>
2019-02-17 17:22:07 -0500

    fix build dependencies

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ plugin-ipfs:
 # build ipfs daemon
 .PHONY: ipfs
 ipfs:
-	( cd vendor/github.com/ipfs/go-ipfs/cmd/ipfs ; go build -o ../../../../../../build/ipfs ; cp ipfs $(GOPATH)/bin)
+	( cd vendor/github.com/ipfs/go-ipfs/cmd/ipfs ; go build -o ../../../../../../build/ipfs ; cp ../../../../../../build/ipfs $(GOPATH)/bin)
 
 # clean up files
 .PHONY: clean
@@ -40,18 +40,10 @@ gx:
 	go get -u github.com/whyrusleeping/gx-go
 	go get -u github.com/karalabe/ungx
 
-
 # build libp2p plugin
 .PHONY: plugin-libp2p
 plugin-libp2p:
 	$(GOCC) build -a -tags libp2p -buildmode=plugin
-
-# fetch dependencies
-.PHONY: deps
-deps:
-	go get -u github.com/RTradeLtd/go-ipfs-plugin-i2p-bootstrap
-	go get -u github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway/config
-	#$(GX_GO_PATH) get github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway
 
 # build i2p  folder
 .PHONY: build-i2p
@@ -61,16 +53,18 @@ build-i2p:
 # format i2p and config golang files
 .PHONY: fmt
 fmt:
-	find ./i2p -name '*.go' -exec gofmt -w {} \;
+	find ./i2p ./config -name '*.go' -exec gofmt -w {} \;
 
 # run tests
 .PHONY: test
 test:
+	go test ./config -v
 	go test ./i2p -v
 
 # vet go code
 .PHONY: vet
 vet:
+	go vet ./config
 	go vet ./i2p
 
 # import gx ipfs
--- i2p/i2p.go
+++ i2p/i2p.go
@@ -1,49 +1,48 @@
 package i2pbootstrap
 
 import (
-	"log"
-	"os"
+	//"log"
+	//"os"
 
 	config "gx/ipfs/QmTbcMKv6GU3fxhnNcbzYChdox9Fdd7VpucM3PQ7UWjX3D/go-ipfs-config"
 
 	"github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway/config"
-	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	plugin "github.com/ipfs/go-ipfs/plugin"
-	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
+	//fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
+	coreiface "gx/ipfs/QmNmqKNivNTN11HrKWJYt29n6Z2fuzkeDheQV62dbxNuLb/interface-go-ipfs-core"
 )
 
-// I2PGatePlugin is a structure containing information which is used for
+
+// I2PBootstrapPlugin is a structure containing information which is used for
 // setting up an i2p tunnel that connects an IPFS bootstrap to a tunnel over i2p.
-type I2PGatePlugin struct {
+type I2PBootstrapPlugin struct {
 	configPath    string
 	config        *config.Config
 	i2pconfigPath string
 	i2pconfig     *i2pgateconfig.Config
 
-	forwardHTTP  string
-	forwardRPC   string
-	forwardSwarm string
+	connectBootstraps []string
 }
 
 // I2PType will be used to identify this as the i2p bootstrap plugin to things
 // that use it.
 var I2PType = "i2pbootstrap"
 
-var _ plugin.PluginDaemon = (*I2PGatePlugin)(nil)
+var _ plugin.PluginDaemon = (*I2PBootstrapPlugin)(nil)
 
 // Name returns the plugin's name, satisfying the plugin.Plugin interface.
-func (*I2PGatePlugin) Name() string {
+func (*I2PBootstrapPlugin) Name() string {
 	return "fwd-i2pbootstrap"
 }
 
 // Version returns the plugin's version, satisfying the plugin.Plugin interface.
-func (*I2PGatePlugin) Version() string {
+func (*I2PBootstrapPlugin) Version() string {
 	return "0.0.0"
 }
 
 // Init initializes plugin, satisfying the plugin.Plugin interface. Put any
 // initialization logic here.
-func (i *I2PGatePlugin) Init() error {
+func (i *I2PBootstrapPlugin) Init() error {
 	/*i := Setup()
 	    if err != nil {
 			return nil, err
@@ -51,13 +50,13 @@ func (i *I2PGatePlugin) Init() error {
 	return nil
 }
 
-// Setup creates an I2PGatePlugin and config file, but it doesn't start
+// Setup creates an I2PBootstrapPlugin and config file, but it doesn't start
 // any tunnels.
-func Setup() (*I2PGatePlugin, error) {
+func Setup() (*I2PBootstrapPlugin, error) {
 	return nil, nil
 }
 
-func (i *I2PGatePlugin) swarmString() string {
+func (i *I2PBootstrapPlugin) swarmString() string {
 	swarmaddressbytes := ""
 	for _, v := range i.config.Addresses.Swarm {
 		swarmaddressbytes += v
@@ -65,36 +64,37 @@ func (i *I2PGatePlugin) swarmString() string {
 	return i2pgateconfig.Unquote(string(swarmaddressbytes))
 }
 
-func (i *I2PGatePlugin) idString() string {
+func (i *I2PBootstrapPlugin) idString() string {
 	idbytes := i.config.Identity.PeerID
 	return i2pgateconfig.Unquote(string(idbytes))
 }
 
 // I2PTypeName returns I2PType
-func (*I2PGatePlugin) I2PTypeName() string {
+func (*I2PBootstrapPlugin) I2PTypeName() string {
 	return I2PType
 }
 
-func (i *I2PGatePlugin) falseStart() error {
+func (i *I2PBootstrapPlugin) falseStart() error {
 	i2p, err := Setup()
 	if err != nil {
 		return err
 	}
+    i2p.ConnectBootstraps()
 
 	return nil
 }
 
 // Start starts the tunnels and also satisfies the Daemon plugin interface
-func (i *I2PGatePlugin) Start(coreiface.CoreAPI) error {
+func (i *I2PBootstrapPlugin) Start(coreiface.CoreAPI) error {
 	i2p, err := Setup()
 	if err != nil {
 		return err
 	}
-
+    i2p.ConnectBootstraps()
 	return nil
 }
 
 // Close satisfies the Daemon plugin interface
-func (*I2PGatePlugin) Close() error {
+func (*I2PBootstrapPlugin) Close() error {
 	return nil
 }

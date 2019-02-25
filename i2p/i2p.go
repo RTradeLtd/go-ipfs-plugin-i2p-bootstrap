d14775c551d9cedd27780b50757cde7c03732479
idk <problemsolver@openmailbox.org>
2019-02-25 17:01:15 -0500

    experimental auto-bootstrap

--- i2p/i2p.go
+++ i2p/i2p.go
@@ -1,15 +1,17 @@
 package i2pbootstrap
 
 import (
-	//"log"
-	//"os"
+	"log"
+	"os"
 
-	config "gx/ipfs/QmTbcMKv6GU3fxhnNcbzYChdox9Fdd7VpucM3PQ7UWjX3D/go-ipfs-config"
+    //config "gx/ipfs/QmTbcMKv6GU3fxhnNcbzYChdox9Fdd7VpucM3PQ7UWjX3D/go-ipfs-config"
+	config "gx/ipfs/QmRLDpfN3yCpHx4C6wwTkrFFK6bNxzBkgDbJPRsb5VLMQ2/go-ipfs-config"
 
 	"github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway/config"
 	plugin "github.com/ipfs/go-ipfs/plugin"
-	//fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
-	coreiface "gx/ipfs/QmNmqKNivNTN11HrKWJYt29n6Z2fuzkeDheQV62dbxNuLb/interface-go-ipfs-core"
+	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
+	//coreiface "gx/ipfs/QmNmqKNivNTN11HrKWJYt29n6Z2fuzkeDheQV62dbxNuLb/interface-go-ipfs-core"
+    coreiface "gx/ipfs/QmeWKXQfEqbtUDCiQBAHzSZDja9br5LdPgk8eHu86oJxgr/interface-go-ipfs-core"
 )
 
 
@@ -53,15 +55,44 @@ func (i *I2PBootstrapPlugin) Init() error {
 // Setup creates an I2PBootstrapPlugin and config file, but it doesn't start
 // any tunnels.
 func Setup() (*I2PBootstrapPlugin, error) {
-	return nil, nil
+    var err error
+	var i I2PBootstrapPlugin
+	i.configPath, err = fsrepo.BestKnownPath()
+	if err != nil {
+		return nil, err
+	}
+	err = os.Setenv("KEYS_PATH", i.configPath)
+	if err != nil {
+		return nil, err
+	}
+	i.config, err = fsrepo.ConfigAt(i.configPath)
+	if err != nil {
+		return nil, err
+	}
+	i.connectBootstraps = i.bootstrapStrings()
+	log.Println("Prepared to connect:", i.connectBootstraps)
+	i.i2pconfig, err = i2pgateconfig.ConfigAt(i.configPath)
+	if err != nil {
+		return nil, err
+	}
+	err = i2pgateconfig.AddressesBootstrap(i.connectBootstraps, i.i2pconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	i.i2pconfig, err = i.i2pconfig.Save(i.configPath)
+	if err != nil {
+		return nil, err
+	}
+	return &i, nil
 }
 
-func (i *I2PBootstrapPlugin) swarmString() string {
-	swarmaddressbytes := ""
-	for _, v := range i.config.Addresses.Swarm {
-		swarmaddressbytes += v
+func (i *I2PBootstrapPlugin) bootstrapStrings() []string {
+	var addresses []string
+	for _, v := range i.connectBootstraps {
+		addresses = append(addresses, i2pgateconfig.Unquote(v))
 	}
-	return i2pgateconfig.Unquote(string(swarmaddressbytes))
+	return addresses
 }
 
 func (i *I2PBootstrapPlugin) idString() string {

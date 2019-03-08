a2fa92d2604a1da334a2320d45a9f627c09057eb
idk <idk@idk.local>
2019-03-07 23:36:37 -0500

    remove dep constraint for go-ipfs-plugin-i2p-gateway

--- plugin.go
+++ plugin.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	i2p "github.com/RTradeLtd/go-ipfs-plugin-i2p-bootstrap/i2p"
+	i2p "github.com/RTradeLtd/go-ipfs-plugin-i2p-gateway/i2p"
+	bootstrapi2p "github.com/RTradeLtd/go-ipfs-plugin-i2p-bootstrap/i2p"
 	plugin "github.com/ipfs/go-ipfs/plugin"
 )
 
 // Plugins is an exported list of plugins that will be loaded by go-ipfs.
 var Plugins = []plugin.Plugin{
-	&i2p.I2PBootstrapPlugin{},
+	&bootstrapi2p.I2PBootstrapPlugin{},
+    &i2p.I2PGatePlugin{},
 }
